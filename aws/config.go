bb87594c487fbbc459629fddf691fb39c11acd41
Edward J <edw@defang.io>
2024-04-05 14:45:08 -0700

    Remove the hardcoded region

--- aws/config.go
+++ aws/config.go
@@ -9,8 +9,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
-var LoadConfig = sync.OnceValue[awssdk.Config](func() awssdk.Config {
-	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion("us-west-2"))
+var LoadConfig = sync.OnceValue(func() awssdk.Config {
+	cfg, err := config.LoadDefaultConfig(context.Background())
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
